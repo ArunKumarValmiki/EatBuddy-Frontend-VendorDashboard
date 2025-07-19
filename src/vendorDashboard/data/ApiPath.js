1b8cd18cf6658afe625dc4f6f2825e4760dfcec8
ArunKumarValmiki <valmikiarunkumar5@gmail.com>
2025-07-19 13:40:40 +0530

    Update ApiPath.js

--- src/vendorDashboard/data/ApiPath.js
+++ src/vendorDashboard/data/ApiPath.js
@@ -1,5 +1,5 @@
 
 // export const API_URL = 'http://localhost:8000' 
 
-export const API_URL = 'https://backend-nodejs-eatbuddy.onrender.com'
+export const API_URL = 'https://eatbuddy-backend-vendordashboard.onrender.com'
 
